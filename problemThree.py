c0ede985b33be7a02049f01fc99e2cddf1b0f20e
Matthew Sunner <sunner.matt@gmail.com>
2023-10-22 12:16:54 -0500

    Solved Problem #3
    Solved and verified the answer to problem number three.

--- problemThree.py
+++ problemThree.py
@@ -3,21 +3,33 @@ Euler Project - Problem Three
 
 The prime factors of 13195 are 5, 7, 13 and 29.
 
-What is the largest prime factor of the number 600851475143 ?
+What is the largest prime factor of the number 600851475143?
 
 https://projecteuler.net/problem=3
 
 """
+num = 20
 
-N = x^a + Y^b + Z^c
+def prime_factors(n):
+    factor_list = []
+    div = 2
 
-
+    while n >1:
+        while n % div == 0:
+            factor_list.append(div)
+            n //= div
+        div += 1
+    
+    return factor_list
 
 
 if __name__ == '__main__':
-    main()
+    num = 600851475143
+    
+    prime_list = prime_factors(num)
 
+    print(max(prime_list))
 
 """
-The Result is:
+The Result is: 6857
 """
