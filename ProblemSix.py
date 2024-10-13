70060e87c86ac8b72a88cf9eaf18a0d4890ef6e0
Matthew Sunner <sunner.matt@gmail.com>
2024-10-12 23:30:33 -0500

    Rebuild Problem Six in OOP Style

--- ProblemSix.py
+++ ProblemSix.py
@@ -10,39 +10,47 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
-def sum_natural_squares(max_val):
-    max_val += 1
-    res_list = []
 
-    for i in range(max_val):
-        res_list.append(i**2)
+class SquareDiff:
+    def __init__(self, max_val: int):
+        self.max_val = max_val
+
+
+    def sum_natural_squares(self):
+        max_val = self.max_val + 1
+        res_list = []
+
+        for i in range(max_val):
+            res_list.append(i**2)
+        
+        result = sum(res_list)
+
+        return result
+
+
+    def square_natural_list(self):
+        max_val = self.max_val + 1
+        res_list = []
+
+        for i in range(max_val):
+            res_list.append(i)
+
+        sum_list = sum(res_list)
+
+        result = sum_list ** 2
+
+        return result
+
     
-    result = sum(res_list)
-
-    return result
-
-
-def square_natural_list(max_val):
-    max_val += 1
-    res_list = []
-
-    for i in range(max_val):
-        res_list.append(i)
-
-    sum_list = sum(res_list)
-
-    result = sum_list ** 2
-
-    return result
+    def final_answer(self):
+        return self.square_natural_list() - self.sum_natural_squares()
 
 
 # Calcualte the Answer
 max_val = 100
 
-answer = square_natural_list(max_val) - sum_natural_squares(max_val)
-print(sum_natural_squares(max_val))
-print(square_natural_list(max_val))
-print(answer)
+answer = SquareDiff(max_val)
+print(answer.final_answer())
 
 
 """
