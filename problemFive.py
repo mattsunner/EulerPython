331ca6b859ce793355a284414e4f281e2ce7d17e
Matthew Sunner <sunner.matt@gmail.com>
2024-10-12 22:54:01 -0500

    Project 5 Ugly Solve

--- problemFive.py
+++ problemFive.py
@@ -9,7 +9,26 @@ https://projecteuler.net/problem=5
 
 """
 
+test_num = 1
+
+while True:
+    if test_num % 20 == 0:
+        if test_num % 19 == 0:
+            if test_num % 18 == 0:
+                if test_num % 17 == 0:
+                    if test_num % 16 == 0:
+                        if test_num % 15 == 0:
+                            if test_num % 14 == 0:
+                                if test_num % 13 == 0:
+                                    if test_num % 12 == 0:
+                                        if test_num % 11 == 0:
+                                            print(f'Result Found: {test_num}')
+                                            break
+    test_num += 1
+
 
 """ 
-Answer: 
+Answer: 232,792,560 
+
+Note: this is a TERRIBLE solution...
 """
