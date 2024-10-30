1fd74e0cee342a735fa939e68c41df92dec002d8
Matthew Sunner <sunner.matt@gmail.com>
2024-10-29 22:19:35 -0500

    Solve Problem Seven
    Solved problem seven in an OOP style while utilizing command line
    arguments.

--- ProblemSeven.py
+++ ProblemSeven.py
@@ -5,32 +5,35 @@ Calculate the 1,000th Prime number
 
 """
 
+import sys
+
 class PrimeCalc:
     def __init__(self, nth_prime):
         self.nth_prime = nth_prime
 
-    
     def calculate(self):
-        lower = 1
-        range_val = self.nth_prime
         primes = []
-       
-        for num in range(lower, range_val + 1):
-           if num > 1:
-               for i in range(2, num):
-                    if (num % i) == 0:
-                        break
-                    else:
-                        print(num)
+        num = 2
+        while len(primes) < self.nth_prime:
+            for prime in primes:
+                if num % prime == 0:
+                    break
+            else:
+                primes.append(num)
+            num += 1
+        return primes[-1]
 
-        
-test_val = 1000
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: Include a Number of Prime Desired")
+        sys.exit(1)
+
+test_val = int(sys.argv[1])
 
 calc = PrimeCalc(test_val)
-
 print(calc.calculate())
 
-
 """
-Answer: 
+Answer: 104,743
 """
